ed13faedc2dafa4a00ef49121b8ff7f8fb0dfcc8
Wed20190109 <wed20190109@outlook.com>
2025-11-17 11:08:09 +0800

    查找链表中间节点

---
+++ Date11_17_2025_链表的中间节点/Date11_17_2025_链表的中间节点/Source.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    ;
+}
+
+ 
+  struct ListNode {
+      int val;
+      struct ListNode *next;
+  };
+ 
+struct ListNode* middleNode(struct ListNode* head) {
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+    }
+    return slow;
+}

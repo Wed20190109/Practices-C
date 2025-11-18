c8254bbcb4cc4e4056831331061ad669929ff0f9
Wed20190109 <wed20190109@outlook.com>
2025-11-18 11:13:23 +0800

    Update

---
+++ Date11_18_2025_合并两个有序链表/Date11_18_2025_合并两个有序链表/Source.c
@@ -0,0 +1,95 @@
+﻿#include <stdio.h>
+/*
+https://leetcode.cn/problems/merge-two-sorted-lists/description/
+
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+
+
+示例 1：
+
+
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+示例 2：
+
+输入：l1 = [], l2 = []
+输出：[]
+示例 3：
+
+输入：l1 = [], l2 = [0]
+输出：[0]
+
+
+提示：
+
+两个链表的节点数目范围是 [0, 50]
+-100 <= Node.val <= 100
+l1 和 l2 均按 非递减顺序 排列
+*/
+
+
+ struct ListNode {
+      int val;
+      struct ListNode *next;
+ };
+ 
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2)
+{
+
+    if (list1 == NULL)
+    {
+        return list2;
+    }
+    if (list2 == NULL)
+    {
+        return list1;
+    }
+    struct ListNode* l1 = list1;
+    struct ListNode* l2 = list2;
+
+
+    struct ListNode* head = NULL;
+    struct ListNode* tail = NULL;
+
+
+    while (l1 && l2)
+    {
+        if (l1->val < l2->val)
+        {
+            if (head == NULL)
+            {
+                head = tail = l1;
+            }
+            else
+            {
+                tail->next = l1;
+                tail = tail->next;
+            }
+            l1 = l1->next;
+        }
+        else
+        {
+            if (head == NULL)
+            {
+                head = tail = l2;
+            }
+            else
+            {
+                tail->next = l2;
+                tail = tail->next;
+            }
+            l2 = l2->next;
+        }
+        if (l2)
+        {
+            tail->next = l2;
+        }
+        if (l1)
+        {
+            tail->next = l1;
+        }
+
+    }
+    return head;
+}
